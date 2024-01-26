26017a2bfebee261f6cf90cb992c4ff04da4a034
raj123shashi <39792479+raj123shashi@users.noreply.github.com>
2024-01-26 17:43:45 +0530

    Add files via upload

---
+++ spring-boot-ecommerse/src/main/java/com/luv2code/ecommerse/SpringBootEcommerseApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerseApplication.class, args);
+	}
+
+}
